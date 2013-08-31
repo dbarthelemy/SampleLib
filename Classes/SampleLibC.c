349299d6efbf9096667be91a384b141b6f51488d
Barthelemy David <dbarthelemy@imakeit4u.com>
2013-08-31 18:30:23 +0200

    O(n) implementation of the C Sample Algo
    Number of iteration for the two implementations:
       input1: 37 O(n) / 2 O(n2)
       input2: 16 O(n) / 1 O(n2)
       input3: 12 O(n) / 6 O(n2)
       input4: 36 O(n) / 45 O(n2)
       input5: 76 O(n) / 190 O(n2)

--- Classes/SampleLibC.c
+++ Classes/SampleLibC.c
@@ -7,17 +7,20 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "SampleLibC.h"
 
 static Result findMaxDistanceOn2(int[], int);
 static Result findMaxDistanceOn(int[], int);
+static int max(int, int);
+static int min(int, int);
 
 // Public functions
 Result findMaxDistance(int input[], int size) {
-    return findMaxDistanceOn2(input, size);
+    return findMaxDistanceOn(input, size);
 }
 
-// Private function
+// Private functions
 Result findMaxDistanceOn2(int input[], int size) {
     int n = size;
     int i = 0;
@@ -49,5 +52,54 @@ Result findMaxDistanceOn2(int input[], int size) {
 }
 
 Result findMaxDistanceOn(int input[], int size) {
-    // TODO
+    int n = size;
+    int i = 0;
+    int j = n-1;
+    Result result = {i, j, 0, 0};
+
+    // Build the array which store the minimum values starting from the first element
+    int *minArray = malloc(sizeof(int) * n);
+    minArray[i] = input[i];
+    for (i = 1; i < n; i++) {
+        minArray[i] = min(input[i], minArray[i-1]);
+        result.iteration++;
+    }
+
+    // Build Max Index Array starting from the last element
+    int *maxArray = malloc(sizeof(int) * n);
+    maxArray[j] = input[j];
+    for (j = n-2; j >= 0; j--) {
+        maxArray[j] = max(input[j], maxArray[j+1]);
+        result.iteration++;
+    }
+    
+    // Find the optimum j-i value by scanning the two arrays form their first elements
+    i = 0;
+    j = 0;
+    int d = -1;
+    while ((j < n) && (i < n)) {
+        if (minArray[i] < maxArray[j]) {
+            if ((j-i) > d) {
+                result.i = i;
+                result.j = j;
+                result.d = j-i;
+            }
+            d = max(d, j-i);
+            j++;
+        }
+        else {
+            i++;
+        }
+        result.iteration++;
+    }
+    
+    return result;
+}
+
+int max(int x, int y) {
+    return (x > y) ? x : y;
+}
+
+int min(int x, int y) {
+    return (x < y) ? x : y;
 }
--- SampleApp/SampleAppTests/SampleAppTests.m
+++ SampleApp/SampleAppTests/SampleAppTests.m
@@ -75,23 +75,33 @@
 {
     int input1[] = {9, 2, 3, 4, 5, 6, 7, 8, 18, 0}; // Output: 8 ( j = 8, i = 0)
     Result result1 = findMaxDistance(input1, ARRAYSIZE(input1));
-    STAssertEquals(result1.d, 8, @"Input 1 failed");
+    STAssertEquals(result1.d, 8, @"Input 1 failed (d)");
+    STAssertEquals(result1.i, 0, @"Input 1 failed (i)");
+    NSLog(@"d = %d ( j = %d, i = %d) after %d iterations", result1.d, result1.j, result1.i, result1.iteration);
     
     int input2[] = {1, 2, 3, 4, 5, 6}; // Output: 5 (j = 5, i = 0)
     Result result2 = findMaxDistance(input2, ARRAYSIZE(input2));
-    STAssertEquals(result2.d, 5, @"Input 2 failed");
+    STAssertEquals(result2.d, 5, @"Input 2 failed (d)");
+    STAssertEquals(result2.i, 0, @"Input 2 failed (i)");
+    NSLog(@"d = %d ( j = %d, i = %d) after %d iterations", result2.d, result2.j, result2.i, result2.iteration);
     
     int input3[] = {20, 20, 18, 20}; // Output: 1 ( j = 3, i = 2)
     Result result3 = findMaxDistance(input3, ARRAYSIZE(input3));
-    STAssertEquals(result3.d, 1, @"Input 3 failed");
+    STAssertEquals(result3.d, 1, @"Input 3 failed (d)");
+    STAssertEquals(result3.i, 2, @"Input 3 failed (i)");
+    NSLog(@"d = %d ( j = %d, i = %d) after %d iterations", result3.d, result3.j, result3.i, result3.iteration);
     
     int input4[] = {20, 20, 20, 20, 20, 20, 20, 20, 18, 20}; // Output: 1 ( j = 9, i = 8)
     Result result4 = findMaxDistance(input4, ARRAYSIZE(input4));
-    STAssertEquals(result4.d, 1, @"Input 4 failed");
+    STAssertEquals(result4.d, 1, @"Input 4 failed (d)");
+    STAssertEquals(result4.i, 8, @"Input 4 failed (i)");
+    NSLog(@"d = %d ( j = %d, i = %d) after %d iterations", result4.d, result4.j, result4.i, result4.iteration);
     
     int input5[] = {20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 18, 20}; // Output: 1 ( j = 19, i = 18)
     Result result5 = findMaxDistance(input5, ARRAYSIZE(input5));
-    STAssertEquals(result5.d, 1, @"Input 5 failed");
+    STAssertEquals(result5.d, 1, @"Input 5 failed (d)");
+    STAssertEquals(result5.i, 18, @"Input 5 failed (i)");
+    NSLog(@"d = %d ( j = %d, i = %d) after %d iterations", result5.d, result5.j, result5.i, result5.iteration);
 }
 
 - (void)testZZZ
